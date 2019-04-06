0fbe53a89f08c79f376f18461c96bc2e4b9b8329
Edward <e-budianto@users.noreply.github.com>
2019-04-06 12:52:27 +0700

    with ultrasonic

--- uctronics_robot_car.ino
+++ uctronics_robot_car.ino
@@ -1,203 +1,176 @@
+/*
+  HG7881_Motor_Driver_Example - Arduino sketch
+   
+  This example shows how to drive a motor with using HG7881 (L9110) Dual
+  Channel Motor Driver Module.  For simplicity, this example shows how to
+  drive a single motor.  Both channels work the same way.
+   
+  This example is meant to illustrate how to operate the motor driver
+  and is not intended to be elegant, efficient or useful.
+   
+  Connections:
+   
+    Arduino digital output D10 to motor driver input B-IA.
+    Arduino digital output D11 to motor driver input B-IB.
+    Motor driver VCC to operating voltage 5V.
+    Motor driver GND to common ground.
+    Motor driver MOTOR B screw terminals to a small motor.
+     
+  Related Banana Robotics items:
+   
+    BR010038 HG7881 (L9110) Dual Channel Motor Driver Module
+    https://www.BananaRobotics.com/shop/HG7881-(L9110)-Dual-Channel-Motor-Driver-Module
+ 
+  https://www.BananaRobotics.com
+*/
+ 
+// wired connections
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Servo.h>
 
+//#define HG7881_B_IA 16 // D10 --> Motor B Input A --> MOTOR B +
+//#define HG7881_B_IB 4 // D11 --> Motor B Input B --> MOTOR B -
+//#define HG7881_A_IA 5 // D10 --> Motor B Input A --> MOTOR B +
+//#define HG7881_A_IB 0 // D11 --> Motor B Input B --> MOTOR B -
+
+#define HG7881_A_IA 16 // D0 --> Motor B Input A --> MOTOR B +
+#define HG7881_B_IA 5 // D1 --> Motor B Input A --> MOTOR B +
+#define HG7881_A_IB 4 // D2 --> Motor B Input B --> MOTOR B -
+#define HG7881_B_IB 0 // D3 --> Motor B Input B --> MOTOR B -
+
+// functional connections
+#define MOTOR_B_PWM HG7881_B_IA // Motor B PWM Speed
+#define MOTOR_B_DIR HG7881_B_IB // Motor B Direction
+#define MOTOR_A_PWM HG7881_A_IA // Motor B PWM Speed
+#define MOTOR_A_DIR HG7881_A_IB // Motor B Direction
+
 //HC-SR04 (5V) - ultrasonic sensor to measure distance
-#define SONAR_TRIG_PIN A5 
-#define SONAR_ECHO_PIN 2
-#define SONAR_INTERRUPT 0 //Note: pin 2
+#define SONAR_TRIG_PIN 14  // D5
+#define SONAR_ECHO_PIN 12  // D6
+#define SONAR_INTERRUPT 13 //Note: pin D7
+//SG90 9G Mini Servo (5V)
+#define SERVO_PIN 2 //D4 
+
+
+// the actual values for "fast" and "slow" depend on the motor
+#define PWM_SLOW 50  // arbitrary slow speed PWM duty cycle
+#define PWM_FAST 200 // arbitrary fast speed PWM duty cycle
+#define DIR_DELAY 1000 // brief delay for abrupt motor changes
+ 
+const char *ssid = "Home";
+const char *password = "edward77";
+const char *mqtt_server = "192.168.2.112";
+const char *userName = "mqtt_user";
+const char *passWord = "edward";
+
 volatile int distance = -1;
 volatile long distCalcStartTime = 0;
 
-//L293D Motor Drive
-#define MOTOR1_PWM_PIN 11
-#define MOTOR2_PWM_PIN 3
-#define MOTOR3_PWM_PIN 6
-#define MOTOR4_PWM_PIN 5
-#define SHIFTREG_LATCH_PIN 12
-#define SHIFTREG_CLK_PIN 4
-#define SHIFTREG_ENABLE_PIN 7
-#define SHIFTREG_DATA_PIN 8
-//74HC595 shift register - used to set the direction of the bridge driver
-#define MOTOR1_A 2
-#define MOTOR1_B 3
-#define MOTOR2_A 1
-#define MOTOR2_B 4
-#define MOTOR3_A 5
-#define MOTOR3_B 7
-#define MOTOR4_A 0
-#define MOTOR4_B 6
-//Codes for motor usage
-#define FORWARD 1
-#define BACKWARD 2
-#define BRAKE 3
-#define RELEASE 4
-#define MAX_PWM 255
-#define MIN_PWM 0
-#define NO_PWM -1
-#define LEFT_MOTOR 3
-#define RIGHT_MOTOR 4
-
-//SG90 9G Mini Servo (5V)
-#define SERVO_PIN 10
-//#define SERVO2_PIN 9 (not used... yet)
-
 Servo servo;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-void setup()
+
+String switch1;
+String strTopic;
+String strPayload;
+int deg;
+
+long lastMsg=0;
+
+void Stop()
 {
-   //HC-SR04
-   pinMode(SONAR_TRIG_PIN, OUTPUT);
-   pinMode(SONAR_ECHO_PIN, INPUT);
-   //interrupt when SONAR_ECHO_PIN changes; used instead of pulseIn(SONAR_ECHO_PIN, HIGH);
-   attachInterrupt(SONAR_INTERRUPT, calculateDistance, CHANGE);
-
-   //L293D Motor Drive - shift register
-   pinMode(SHIFTREG_LATCH_PIN, OUTPUT);
-   pinMode(SHIFTREG_ENABLE_PIN, OUTPUT);
-   pinMode(SHIFTREG_DATA_PIN, OUTPUT);
-   pinMode(SHIFTREG_CLK_PIN, OUTPUT);
-   digitalWrite(SHIFTREG_DATA_PIN, LOW);
-   digitalWrite(SHIFTREG_LATCH_PIN, LOW);
-   digitalWrite(SHIFTREG_CLK_PIN, LOW);
-   digitalWrite(SHIFTREG_ENABLE_PIN, LOW);
-     
-   //SG90
-   servo.attach(SERVO_PIN);
-   moveServo(90);
-   
-   Serial.begin(9600);
+        digitalWrite( MOTOR_B_DIR, LOW );
+        digitalWrite( MOTOR_A_DIR, LOW );
+        digitalWrite( MOTOR_B_PWM, LOW );
+        digitalWrite( MOTOR_A_PWM, LOW );
 }
 
-void moveMotor(int motorNumber, int command, int speed)
-{
-  int motorA, motorB;
-  switch(motorNumber)
-  {
-    case 1:
-      motorA = MOTOR1_A;
-      motorB = MOTOR1_B;
-      break;
-    case 2:
-      motorA = MOTOR2_A;
-      motorB = MOTOR2_B;
-      break;
-    case 3:
-      motorA = MOTOR3_A;
-      motorB = MOTOR3_B;
-      break;
-    case 4:
-      motorA = MOTOR4_A;
-      motorB = MOTOR4_B;
-      break;
-    default:
-      return;
-  }
 
-  switch(command)
-  {
-    case FORWARD:
-      setMotorOutput(motorA, HIGH, speed);
-      setMotorOutput(motorB, LOW, NO_PWM);
-      break;
-    case BACKWARD:
-      setMotorOutput(motorA, LOW, speed);
-      setMotorOutput(motorB, HIGH, NO_PWM);
-      break;
-    case BRAKE:
-      setMotorOutput(motorA, LOW, MAX_PWM);
-      setMotorOutput(motorB, LOW, NO_PWM);
-      break;
-    case RELEASE:
-      setMotorOutput(motorA, LOW, MIN_PWM);
-      setMotorOutput(motorB, LOW, NO_PWM);
-      break;
-    default:
-      break;
-  }
+void moveForward(float seconds)
+{
+  lastMsg = millis();
+  while (millis() - lastMsg < (seconds * 1000))
+     {
+        digitalWrite( MOTOR_B_DIR, HIGH ); // direction = forward
+        digitalWrite( MOTOR_B_PWM, LOW ); // direction = forward
+        digitalWrite( MOTOR_A_DIR, HIGH ); // direction = forward
+        digitalWrite( MOTOR_A_PWM, LOW ); // direction = forward
+        yield();
+     }
+
 }
 
-void setMotorOutput(int output, int highOrLow, int speed)
+void moveBackward(float seconds)
 {
-  int motorPWMPin;
-  switch(output)
-  {
-    case MOTOR1_A:
-    case MOTOR1_B:
-      motorPWMPin = MOTOR1_PWM_PIN;
-      break;
-    case MOTOR2_A:
-    case MOTOR2_B:
-      motorPWMPin = MOTOR2_PWM_PIN;
-      break;
-    case MOTOR3_A:
-    case MOTOR3_B:
-      motorPWMPin = MOTOR3_PWM_PIN;
-      break;
-    case MOTOR4_A:
-    case MOTOR4_B:
-      motorPWMPin = MOTOR4_PWM_PIN;
-      break;
-    default:
-      return;
-  }
+  lastMsg = millis();
+  while (millis() - lastMsg < (seconds * 1000))
+     {
+        digitalWrite( MOTOR_B_DIR, LOW ); // direction = reverse
+        digitalWrite( MOTOR_A_DIR, LOW ); // direction = reverse
+        digitalWrite( MOTOR_B_PWM, HIGH ); // direction = reverse
+        digitalWrite( MOTOR_A_PWM, HIGH ); // direction = reverse
+        yield();
+     }
 
-  //Set motor direction with the shift register
-  shiftWrite(output, highOrLow);
-
-  if(speed >= MIN_PWM && speed <= MAX_PWM) {
-    analogWrite(motorPWMPin, speed);
-  }
 }
 
-void shiftWrite(int output, int highOrLow)
+void spinLeft(float seconds)
 {
-  static int latchCopy = 0;
+  lastMsg = millis();
+  while (millis() - lastMsg < (seconds * 1000))
+     {
+        digitalWrite( MOTOR_B_DIR, HIGH );
+        digitalWrite( MOTOR_B_PWM, LOW );
+        digitalWrite( MOTOR_A_DIR, LOW );
+        digitalWrite( MOTOR_A_PWM, HIGH );
+        yield();
+     }
 
-  bitWrite(latchCopy, output, highOrLow);
-
-  //Shift SHIFTREG_CLK_PIN bits as clock pulse.
-  //The 74HC595 shift register uses most-significant-bit (MSB) first.
-  shiftOut(SHIFTREG_DATA_PIN, SHIFTREG_CLK_PIN, MSBFIRST, latchCopy);
-
-  //Generate a latch pulse with SHIFTREG_LATCH_PIN.
-  delayMicroseconds(5);
-  digitalWrite(SHIFTREG_LATCH_PIN, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(SHIFTREG_LATCH_PIN, LOW);
 }
 
-void moveForward(int speed)
+void turnLeft(float seconds)
 {
-   moveMotor(LEFT_MOTOR, FORWARD, speed);
-   moveMotor(RIGHT_MOTOR, FORWARD, speed);
+  lastMsg = millis();
+  while (millis() - lastMsg < (seconds * 1000))
+     {
+        digitalWrite( MOTOR_B_DIR, HIGH );
+        digitalWrite( MOTOR_B_PWM, LOW );
+        digitalWrite( MOTOR_A_DIR, LOW );
+        digitalWrite( MOTOR_A_PWM, LOW );
+        yield();
+     }
+
 }
 
-void moveBackward(int speed)
+void spinRight(float seconds)
 {
-   moveMotor(LEFT_MOTOR, BACKWARD, speed);
-   moveMotor(RIGHT_MOTOR, BACKWARD, speed);
+  lastMsg = millis();
+  while (millis() - lastMsg < (seconds * 1000))
+     {
+        digitalWrite( MOTOR_B_DIR, LOW );
+        digitalWrite( MOTOR_B_PWM, HIGH );
+        digitalWrite( MOTOR_A_DIR, HIGH );
+        digitalWrite( MOTOR_A_PWM, LOW );
+        yield();
+     }
+
 }
 
-void turnLeft(int speed)
+void turnRight(float seconds)
 {
-   moveMotor(LEFT_MOTOR, BACKWARD, speed);
-   moveMotor(RIGHT_MOTOR, FORWARD, speed);
-}
+  lastMsg = millis();
+  while (millis() - lastMsg < (seconds * 1000))
+     {
+        digitalWrite( MOTOR_B_DIR, LOW );
+        digitalWrite( MOTOR_B_PWM, LOW );
+        digitalWrite( MOTOR_A_DIR, HIGH );
+        digitalWrite( MOTOR_A_PWM, LOW );
+        yield();
+     }
 
-void turnRight(int speed)
-{
-   moveMotor(LEFT_MOTOR, FORWARD, speed);
-   moveMotor(RIGHT_MOTOR, BACKWARD, speed);
-}
-
-void stopMoving()
-{
-   moveMotor(LEFT_MOTOR, RELEASE, MIN_PWM);
-   moveMotor(RIGHT_MOTOR, RELEASE, MIN_PWM);
-}
-
-void moveServo(int angle)
-{
-   servo.write(angle); 
 }
 
 void startDistanceCalculation()
@@ -236,8 +209,135 @@ long calculateDistanceNow()
   return (duration/2) / 29.1;
 }
 
+void moveServo(int angle)
+{
+   servo.write(angle); 
+}
+
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("smartcar", userName, passWord)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.subscribe("car/#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  payload[length] = '\0';
+  strTopic = String((char*)topic);
+  if(strTopic == "car/move")
+    {
+    switch1 = String((char*)payload);
+    if(switch1 == "W")
+      {
+         moveForward(2);
+         Stop();
+      }
+    else if(switch1 == "A")
+      {
+         turnLeft(3);
+      }
+    else if(switch1 == "D")
+      {
+         turnRight(3);
+      }
+    else if(switch1 == "Z")
+      {
+        moveBackward(1);
+        Stop();
+      }
+    else if(switch1 == "Q")
+      {
+        spinLeft(1);
+        Stop();
+      }
+    else if(switch1 == "E")
+      {
+        spinRight(1);
+        Stop();
+      }
+    else if(switch1 == "S")
+      {
+        Stop();
+      }
+
+
+    }
+
+}
+
+
+void setup()
+{
+  Serial.begin( 9600 );
+  setup_wifi(); 
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+   //HC-SR04
+   pinMode(SONAR_TRIG_PIN, OUTPUT);
+   pinMode(SONAR_ECHO_PIN, INPUT);
+   //interrupt when SONAR_ECHO_PIN changes; used instead of pulseIn(SONAR_ECHO_PIN, HIGH);
+   attachInterrupt(SONAR_INTERRUPT, calculateDistance, CHANGE);
+
+     
+   //SG90
+   servo.attach(SERVO_PIN);
+   moveServo(90);
+
+  pinMode( MOTOR_B_DIR, OUTPUT );
+  pinMode( MOTOR_B_PWM, OUTPUT );
+  digitalWrite( MOTOR_B_DIR, LOW );
+  digitalWrite( MOTOR_B_PWM, LOW );
+  pinMode( MOTOR_A_DIR, OUTPUT );
+  pinMode( MOTOR_A_PWM, OUTPUT );
+  digitalWrite( MOTOR_A_DIR, LOW );
+  digitalWrite( MOTOR_A_PWM, LOW );
+
+
+}
+
 void loop()
 {
+  if (!client.connected()) {
+    reconnect();
+  }
+
    //Calculate distance
    Serial.print("Distance: ");
    Serial.println(distance);
@@ -247,17 +347,16 @@ void loop()
 
    if(distance > 40) {
       //No obstacles detected - full speed ahead!
-      moveForward(MAX_PWM);
-      delay(15);
+      moveForward(3);
    }
    else if(distance <= 40 && distance > 20) {
       //Upcoming obstacle detected - slow down
-      moveForward(MAX_PWM/2);
-      delay(15);
+      moveForward(1);
+      Stop();
    }
    else {
-      //Detcted obstacle way too close for comfort - backup and turn
-      stopMoving();
+      //Detected obstacle way too close for comfort - backup and turn
+      Stop();
 
       //Scan area
       moveServo(120);
@@ -268,20 +367,23 @@ void loop()
       long rightDist = calculateDistanceNow();
       moveServo(90);
     
-      moveBackward(MAX_PWM);
+      moveBackward(3);
       delay(500);
 
       if(leftDist >= rightDist) {
-        turnLeft(MAX_PWM);
+        spinLeft(2);
       }
       else {
-        turnRight(MAX_PWM);
+        spinRight(2);
       }
       delay(350);
 
-      stopMoving();
+      Stop();
       calculateDistanceNow();
 
       delay(15);
    }
+
+  client.loop();
 }
+
